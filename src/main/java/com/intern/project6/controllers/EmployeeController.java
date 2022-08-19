5f0da96cf1d1d33a2134c1d68de74e7074843792
stefania.iuga <iugastefania26@gmail.com>
2022-08-19 13:32:36 +0300

    removed test endpoint

--- src/main/java/com/intern/project6/controllers/EmployeeController.java
+++ src/main/java/com/intern/project6/controllers/EmployeeController.java
@@ -14,10 +14,7 @@ public class EmployeeController {
 
     @Autowired
     EmployeeService employeeService;
-    @GetMapping("/getHello")
-    public String getHello(){
-        return "Hello!";
-    }
+
     @GetMapping("/getEmployee/{id}")
     public ResponseEntity<Employee> getEmployee(@PathVariable String id){
         Employee employee = employeeService.getEmployee(id);
